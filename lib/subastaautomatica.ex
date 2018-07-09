6ddf51df80d920bf5995f35c8776928d533fa3e1
Hernan <hernan.tomassini@hotmail.com>
2018-07-09 19:01:22 -0300

    Proyecto creado

---
+++ lib/subastaautomatica.ex
@@ -0,0 +1,18 @@
+defmodule SubastaAutomatica do
+  @moduledoc """
+  Documentation for SubastaAutomatica.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SubastaAutomatica.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
