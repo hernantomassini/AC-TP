b03f2c1f99949e706a9aed8a605ddb6d1204dc43
Hernan <hernan.tomassini@hotmail.com>
2018-07-25 16:25:19 -0300

    Fixed test

--- usuario/test/usuario/registry_test.exs
+++ usuario/test/usuario/registry_test.exs
@@ -2,28 +2,23 @@
 defmodule Usuario.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    {:ok, registry} = Usuario.Registry.start_link([])
-    %{registry: registry}
-  end
-
-  test "Crear usuario", %{registry: registry} do
+  test "Crear usuario" do
     usuario1=%Usuario.Struct{}
     usuario2=%Usuario.Struct{}
     usuario1=%{usuario1 | id: 'idUsuario1',ip: "127.0.0.1",puerto: "80"}
     usuario2=%{usuario2 | id: 'idUsuario2',ip: "127.0.33.1",puerto: "80"}
-    Usuario.Registry.create(registry,usuario1)
-    Usuario.Registry.create(registry,usuario2)
+    Usuario.Registry.create(usuario1)
+    Usuario.Registry.create(usuario2)
 
-    dataUsuario1=Usuario.Registry.getEstado(registry,'idUsuario1')
-    dataUsuario2=Usuario.Registry.getEstado(registry,'idUsuario2')
-    assert usuario1 = Usuario.Registry.getEstado(registry,'idUsuario1')
-    assert usuario2 = Usuario.Registry.getEstado(registry,'idUsuario2')
+    dataUsuario1=Usuario.Registry.get_estado('idUsuario1')
+    dataUsuario2=Usuario.Registry.get_estado('idUsuario2')
+    assert usuario1 = Usuario.Registry.get_estado('idUsuario1')
+    assert usuario2 = Usuario.Registry.get_estado('idUsuario2')
 
     IO.puts(inspect(dataUsuario1))
     IO.puts(inspect(dataUsuario2))
 
-   pidUsuario1=Usuario.Registry.getPidUsuario(registry,'idUsuario1')
+   pidUsuario1=Usuario.Registry.get_pid_usuario('idUsuario1')
    IO.inspect(pidUsuario1,label: "PID")
     Usuario.State.crear_subasta(pidUsuario1)
 
