d92aa111d1f5161314c1c594b8ad0365ecf06aa1
camus122 <mu122@msn.com>
2018-07-28 18:54:19 -0300

    se sube implemtnacion de nuemro de reinteintos

--- usuario/lib/estrategia/reintentos.ex
+++ usuario/lib/estrategia/reintentos.ex
@@ -19,18 +19,33 @@ defmodule Estrategia.Reintentos do
 #    GenServer.cast(pidStrategia, {:ejecutar_estrategia_ganar, idUsuario,subasta})
 #  end
 
-  def handle_cast({:ejecutar, idUsuario,subasta}, estado_actual) do
+  def handle_cast({:ejecutar, id_usuario,subasta}, estado_actual) do
     datos=estado_actual.datos
-    cant_reinttos_realizados_subasta=3
-    if(cant_reinttos_realizados_subasta <= datos.cant_reintentos) do
-      IO.puts("USUAROI: #{idUsuario} Estrategia_Reintentos_num: #{cant_reinttos_realizados_subasta} para subasta: #{subasta.id}")
+    mapa_ofertados=estado_actual.ofertasRealizadas
+    cant_reinttos_realizados_subasta=Estrategia.Reintentos.obtener_cantidad_reintentos_subasta(mapa_ofertados,subasta.id)
+    if(cant_reinttos_realizados_subasta < datos.cant_reintentos) do
+      IO.puts("USUAROI: #{id_usuario} Estrategia_Reintentos_num: #{cant_reinttos_realizados_subasta} para subasta: #{subasta.id}")
       precio_ofertado=obtener_precio_a_ofertar(subasta, datos.sumar_al_precio)
       IO.inspect(precio_ofertado,label: "precio_ofertado")
-      response=Usuario.ofertar_subasta(idUsuario, subasta.id, precio_ofertado)
+      response=Usuario.ofertar_subasta(id_usuario, subasta.id, precio_ofertado)
       if(!response.error) do
         #aumentar cantida de reintentos de la subaasta
+        estado_actual=%{estado_actual | ofertasRealizadas: Map.put(mapa_ofertados, subasta.id, cant_reinttos_realizados_subasta+1)}
+        estado_actual.ofertasRealizadas
         {:noreply, estado_actual}
       end
+      else
+        IO.puts("Usuario: #{id_usuario}. Realizo maximo de reintentos para subatas.id= #{subasta.id}. Producto: #{subasta.articuloNombre}}")
+        {:noreply, estado_actual}
+      end
+  end
+
+  def obtener_cantidad_reintentos_subasta(mapa, id_subasta) do
+    if Map.has_key?(mapa, id_subasta) do
+      {:ok,cant_reintentos_realizados}= Map.fetch(mapa, id_subasta)
+      cant_reintentos_realizados
+    else
+      0
     end
   end
 
@@ -48,7 +63,7 @@ defmodule Estrategia.Reintentos do
 
 
   def set_datos_estrategia(pidStrategia, datos) do
-    set_estado(pidStrategia,%{datos: datos, ofertasRealizadas: []})
+    set_estado(pidStrategia,%{datos: datos, ofertasRealizadas: MapSet.new()})
   end
 
   def set_estado(pidStrategia, estado) do
