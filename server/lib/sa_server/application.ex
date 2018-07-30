6a8fb5399d54a83165e0558802fe3b426ddb1350
Hernan <hernan.tomassini@hotmail.com>
2018-07-30 20:28:25 -0300

    Ahora el servidor acepta parametros pero no son obligatorios

--- server/lib/sa_server/application.ex
+++ server/lib/sa_server/application.ex
@@ -3,18 +3,17 @@ defmodule Server.Application do
   use Application
 
   def start(_type, _args) do
-    name="server1"
-    ip="127.0.0.1"
-    port="8085"
-    ip_admin="127.0.0.1"
-    port_admin="1234"
+    name = System.get_env("name") || "server1"
+    ip = System.get_env("ip") || "127.0.0.1"
+    port = System.get_env("port") || "8085"
+    ip_admin = System.get_env("ip_admin") || "127.0.0.1"
+    port_admin =  System.get_env("port_admin") || "1234"
 
     # List all child processes to be supervised
     children = [
       Plug.Adapters.Cowboy.child_spec(scheme: :http, plug: Server.Router, options: [port: String.to_integer(port)]),
       GlobalContext,
       { Task, fn -> AdministradorTask.inicializar(name, ip, port, ip_admin, port_admin) end }
-
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
